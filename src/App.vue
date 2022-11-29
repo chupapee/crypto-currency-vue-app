6b69825950e966d13e0e4298ea1a533540850bc0
ComeAll <kamolkhamidov200002@gmail.com>
2022-11-30 00:22:09 +0500

    configured websocket and rest api

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,148 @@
 <template>
-  <div class="app">
-    <div class="bg-white h-[80vh] overflow-auto flex flex-col w-full max-w-xl mx-auto my-5 border-2 px-3 py-4 rounded-lg">
-      <div class="flex sticky top-0 left-0 right-0">
-        <button class="bg-white w-full px-3 py-4 border-2 border-b-0">Current</button>
-        <button class="bg-gray-500 w-full px-3 py-4 text-white">Done</button>
-      </div>
-      
-      <div class="bg-white flex my-4">
-        <input
-          class="border-2 px-3 py-4 border-solid rounded-lg w-full mr-3 outline-none focus:border-blue-600 hover:border-blue-600 border-blue-400"
-          @keydown.enter="add"
-          type="text"
-          v-model="newTask"
-        >
-        <button class="text-3xl text-white px-10 bg-blue-600 hover:bg-blue-800 duration-150 border-solid border-2 rounded-lg" @click="add">+</button>
-      </div>
-  
-      <div class="bg-white flex flex-col" v-if="tasks.length">
-        <template key="task.id" v-for="task in tasks">
-          <div class="flex py-4 text-lg items-center">
-            <input class="mr-4 h-6 w-6" type="checkbox" @click="task.status = 'checked'">
-            <p class="w-full" :class="task.status === 'checked' ? 'line-through' : 'none'">{{task.text}}</p>
-            <button @click="del(task.id)" class="py-2 px-4 border-red-500 rounded-lg border-solid border-[1px] hover:bg-red-400 hover:text-white">Del</button>
-          </div>
-        </template>
-      </div>
+  <!-- updating ticker -->
+  <div class="flex flex-col">
+    <span class="py-2 text-lg font-medium">Тикер</span>
+    <input
+      v-model="tickerName"
+      @keydown.enter="add"
+      placeholder="Например DOGE"
+      type="text"
+      class="px-4 py-3 mr-4 border-[1px] mb-2 border-solid rounded-lg outline-none hover:border-blue-700 focus:border-blue-700 w-1/4"
+    >
+    <button @click="add" class="baseBtn">Добавить +</button>
+  </div>
+
+  <!-- filter & pagination -->
+  <div class="flex flex-col border-y-2 border-gray-600 border-solid p-4 my-4">
+    <div v-if="tickers.length > 6">
+      <button @click="page = page - 1" v-if="page > 1" class="baseBtn mr-4">Назад</button>
+      <button @click="page = page + 1" v-if="page * 6 < tickers.length" class="baseBtn">Вперед</button>
+    </div>
+    <div class="mt-4">
+      <p class="text-xl font-medium text-gray-600 mb-1">filter:</p>
+      <input
+        v-model="filter"
+        class="px-4 py-3 mr-4 border-[1px] mb-2 border-solid rounded-lg outline-none hover:border-blue-700 focus:border-blue-700 w-1/5"
+      />
     </div>
   </div>
+
+  <!-- list -->
+  <div v-if="tickers.length" class="border-y-2 border-gray-600 border-solid p-4 my-4 grid grid-cols-3 justify-items-center">
+    <template key="ticker.name" v-for="ticker in paginatedTickers">
+      <div class="flex flex-col items-center gap-4 my-2">
+        <p class="uppercase font-medium text-xl text-gray-500">{{ticker.name}} - USD</p>
+        <p class="font-medium text-3xl">{{ticker.price}}</p>
+        <button
+          class="hover:bg-blue-500 hover:text-white px-3 py-2 border-blue-600 rounded-lg border-solid border-[1px]"
+          @click="del(ticker)"
+        >
+          Удалить
+        </button>
+      </div>
+    </template>
+  </div>
+
 </template>
 
 <script>
+import { subscribeToTicker, unSubscribeToTicker } from './api'
+
 export default {
   name: 'App',
   data() {
     return {
-      newTask: '',
-      tasks: []
+      tickerName: '',
+      filter: '',
+
+      tickers: [],
+
+      page: 1,
     }
   },
+  created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams)
+    if(windowData) {
+      this.page = Number(windowData.page ?? 1)
+      this.filter = windowData.filter ?? ''
+    }
+
+    const tickers = this.getFromLocalstorage()
+    if (tickers && tickers.length) {
+      this.tickers = tickers
+      tickers.forEach(ticker => subscribeToTicker(ticker.name, newPrice => {
+        this.updateTicker(ticker, newPrice)
+      }))
+    }
+  },
+
+  computed: {
+    filteredTickers() {
+      if(this.tickers.length > 0) {
+        return this.tickers.filter(({name}) => name.includes(this.filter))
+      }
+      return this.tickers
+    },
+    paginatedTickers() {
+      const start = 6 * (this.page - 1)
+      const end = this.page * 6
+      
+      if(this.tickers.length > 0) {
+        return this.filteredTickers.slice(start, end)
+      }
+      return this.tickers
+    }
+  },
+
   methods: {
+    updateTicker(ticker, newPrice) {
+      this.tickers
+        .filter(t => t.name === ticker.name)
+        .forEach(t => t.price = newPrice)
+      this.setTolocalstorage()
+    },
+      add() {
+      if(this.tickerName) {
+        const currentTicker = {
+          id: Date.now(),
+          name: this.tickerName,
+          price: '-'
+        }
+        
+        this.tickers.push(currentTicker)
+        
+        subscribeToTicker(this.tickerName, newPrice => {
+          this.updateTicker(currentTicker.name, newPrice)
+        })
+        
+        this.setTolocalstorage()
+        this.tickerName = ''
+      }
+    },
+    del(tickerToRemove) {
+      this.tickers = this.tickers.filter(ticker => ticker !== tickerToRemove)
+    },
+    setTolocalstorage() {
+      localStorage.setItem('tickerList', JSON.stringify(this.tickers))
+    },
+    getFromLocalstorage() {
+      return JSON.parse(localStorage.getItem('tickerList'))
+    }
+  },
+  
+  watch: {
     add() {
-      const task = {id: Math.random().toFixed(4), text: this.newTask, status: null}
-      this.tasks.push(task)
-      this.newTask = ''
+      this.page = this.tickers.length
     },
-    del(id) {
-      const tasks = this.tasks.filter(task => task.id !== id )
-      this.tasks = tasks
+    paginatedTickers() {
+      if(this.paginatedTickers.length === 0 && this.page > 1) this.page -= 1
     },
-    check(id) {
-      this.tasks
+    page() {
+      window.history.pushState(null, document.title, `${window.location.pathname}?page=${this.page}&filter=${this.filter}`)
+    },
+    filter() {
+      this.page = 1
+      window.history.pushState(null, document.title, `${window.location.pathname}?page=${this.page}&filter=${this.filter}`)
     }
   }
 }
