796611087109b790f569c494e4e731b1384ca8cf
Kamol <kamolkhamidov200002@gmail.com>
2022-12-13 18:29:21 +0500

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,jsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
